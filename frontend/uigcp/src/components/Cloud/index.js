1579c46277b806cf37e1056b67b60769d1d466f1
KVNP <kvnp179@gmail.com>
2022-12-29 04:00:04 +0530

    working update

--- frontend/uigcp/src/components/Cloud/index.js
+++ frontend/uigcp/src/components/Cloud/index.js
@@ -1,8 +1,89 @@
-import React from "react";
+import React,{useState} from "react";
 import "./index.css";
 import "../bootstrap/bootstrap.min.css";
-import {showProgress} from "./progress";
 const Cloud = () => {
+ const [count, setCount] = useState(0);
+
+    const showProgress = () => {
+      console.log("showing progress...");
+      document.getElementById("progress").style.display = "block";
+      document.getElementById("value").disabled = true;
+      //document.getElementById("submit-button").innerText = "Fetch Status";
+      //document.getElementById("submit-button").setAttribute( "onClick", getProgress );
+      getProgress();
+    }
+
+    const hudson = (data) => {
+      console.log("task finished")
+      const status = JSON.stringify(data);
+      // blurt(
+      //     'Task Successfully completed',
+      //     'Status: ' + status,
+      //     'success'
+      // );
+      document.getElementById("progress").style.display = "none";
+      document.getElementById("value").disabled = false;
+      document.getElementById("submit-button").innerText = "Submit";
+      document.getElementById("submit-button").setAttribute( "onClick", showProgress );
+      document.getElementById("statusArea").innerHTML += JSON.stringify(data.status);
+    }
+
+    const getProgress = () => {
+      console.log("inside get progress");
+      const value = document.querySelector("#value").value;
+
+      const userAction = async (fun) => {
+          const StatusUrl = 'https://us-east1-healthcarepci.cloudfunctions.net/test?message='+value;
+          console.log("value: " + value)
+          const data = {
+            "message": value
+          }
+          const options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            mode: "no-cors"
+          }
+          const response=await fetch(StatusUrl)
+          .then(response => response.json())
+          .then(json => fun(json))
+          .catch((err) => console.log(err));
+      }
+
+      const changeToIdle = async (fun) => {
+        const StatusUrl = 'https://us-east1-healthcarepci.cloudfunctions.net/refresh?message='+value;
+        console.log("In changing to idle:")
+        const options = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          mode: "no-cors"
+        }
+        const response=await fetch(StatusUrl)
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch((err) => console.log(err));
+    }
+
+      const addStatus =  (data)=> {
+        console.log("data: " + JSON.stringify(data));
+        if(JSON.stringify(data.status)=="\"initial\"")
+          document.getElementById("statusArea").innerHTML = "Task successfully added to queue";
+        else if (JSON.stringify(data.status) == "\"Completed\""){ 
+          prompt("Task Completed successfully")
+          changeToIdle();
+        }
+        else if (JSON.stringify(data.status) == "\"Running\""){ 
+        }
+          // document.getElementById("statusArea").innerHTML = JSON.stringify(data);
+          // document.getElementById("status-heading").style.display = "inline";
+          // if(JSON.stringify(data.status)=="\"completed\""){
+          //     //hudson(data);
+          // }
+      }
+      userAction(addStatus);
+    }
+
+
+
   return (
     <React.Fragment>
       <div id="whole">
