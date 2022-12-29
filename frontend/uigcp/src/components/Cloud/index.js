0d82bc1f5cfadba84a6b2f4fc4d37f0f968b15db
KVNP <kvnp179@gmail.com>
2022-12-29 12:28:46 +0530

    react daily

--- frontend/uigcp/src/components/Cloud/index.js
+++ frontend/uigcp/src/components/Cloud/index.js
@@ -1,17 +1,25 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import "./index.css";
 import "../bootstrap/bootstrap.min.css";
+import "./loadingbar.css";
 const Cloud = () => {
- const [count, setCount] = useState(0);
-
+  const [value, setValue] = useState("");
+  const [check,setCheck] = useState(true);
     const showProgress = () => {
       console.log("showing progress...");
       document.getElementById("progress").style.display = "block";
       document.getElementById("value").disabled = true;
-      //document.getElementById("submit-button").innerText = "Fetch Status";
+      document.getElementById("submit-button").innerText = "Check Status";
       //document.getElementById("submit-button").setAttribute( "onClick", getProgress );
       getProgress();
+      setCheck(false)
     }
+
+    const handleChange = (e) => {
+      setValue(e.target.value);
+      localStorage.setItem("inputValue", e.target.value);
+      console.log(localStorage.getItem("inputValue"));
+    };
 
     const hudson = (data) => {
       console.log("task finished")
@@ -24,12 +32,69 @@ const Cloud = () => {
       document.getElementById("progress").style.display = "none";
       document.getElementById("value").disabled = false;
       document.getElementById("submit-button").innerText = "Submit";
-      document.getElementById("submit-button").setAttribute( "onClick", showProgress );
-      document.getElementById("statusArea").innerHTML += JSON.stringify(data.status);
+      //document.getElementById("submit-button").setAttribute( "onClick", showProgress );
+      //document.getElementById("statusArea").innerHTML += JSON.stringify(data.status);
     }
 
+    function getTime(){
+      var today = new Date();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds(); 
+      return [ h, m, s ].join(':')
+    }
+
+    const changeToIdle = async (fun) => {
+      const resetUrl = 'https://us-central1-healthcarepci.cloudfunctions.net/refresh?message='+value;
+      console.log("In changing to idle:")
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        mode: "no-cors"
+      }
+      const response=await fetch(resetUrl,options)
+      .then(response => console.log(response))
+      .catch((err) => console.log(err));
+      setCheck(false)
+  }
+
+    const checkStatus = () => {
+      console.log("inside check status");console.log("Check: ", check);
+      
+      const userActionq = async (fun) => {
+        const StatusUrl = 'https://us-east1-healthcarepci.cloudfunctions.net/StatusCheck?message='+value;
+        console.log("value: " + value)
+        const options = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          mode: "no-cors"
+        }
+        const response=await fetch(StatusUrl,options)
+        .then(response => response.json())
+        .then(json => fun(json))
+        .catch((err) => console.log(err));
+    }
+
+      const addStatusq =  (data)=> {
+        console.log("data: " + JSON.stringify(data));
+        if(JSON.stringify(data.status)=="\"initial\"")
+          document.getElementById("statusArea").innerHTML = "Task successfully added to queue";
+        else if (JSON.stringify(data.status) == "\"completed\""){ 
+          alert("Task Completed successfully")
+          changeToIdle();
+          hudson(data);
+        }
+        else if (JSON.stringify(data.status) == "\"running\""){ 
+          document.getElementById("statusArea").innerHTML = JSON.stringify(data);
+          document.getElementById("status-heading").style.display = "inline";
+        }
+      }
+
+      userActionq(addStatusq);
+    }
     const getProgress = () => {
-      console.log("inside get progress");
+      
+      console.log("inside get progress");console.log("Check: ", check);
       const value = document.querySelector("#value").value;
 
       const userAction = async (fun) => {
@@ -49,29 +114,20 @@ const Cloud = () => {
           .catch((err) => console.log(err));
       }
 
-      const changeToIdle = async (fun) => {
-        const StatusUrl = 'https://us-east1-healthcarepci.cloudfunctions.net/refresh?message='+value;
-        console.log("In changing to idle:")
-        const options = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          mode: "no-cors"
-        }
-        const response=await fetch(StatusUrl)
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch((err) => console.log(err));
-    }
 
       const addStatus =  (data)=> {
         console.log("data: " + JSON.stringify(data));
         if(JSON.stringify(data.status)=="\"initial\"")
           document.getElementById("statusArea").innerHTML = "Task successfully added to queue";
-        else if (JSON.stringify(data.status) == "\"Completed\""){ 
-          prompt("Task Completed successfully")
+        else if (JSON.stringify(data.status) == "\"completed\""){ 
+          alert("Task Completed successfully")
           changeToIdle();
+          hudson(data);
         }
-        else if (JSON.stringify(data.status) == "\"Running\""){ 
+        else if (JSON.stringify(data.status) == "\"running\""){ 
+          alert("Task Already running")
+          document.getElementById("statusArea").innerHTML = JSON.stringify(data);
+          document.getElementById("status-heading").style.display = "inline";
         }
           // document.getElementById("statusArea").innerHTML = JSON.stringify(data);
           // document.getElementById("status-heading").style.display = "inline";
@@ -82,7 +138,19 @@ const Cloud = () => {
       userAction(addStatus);
     }
 
-
+    useEffect(() => {
+      setValue(localStorage.getItem("inputValue"));
+      console.log("Check: ", check);
+      console.log("local: ", localStorage.getItem("inputValue"));
+      if(localStorage.getItem("inputValue") === "" || !localStorage.getItem("inputValue")){
+        console.log("fresh session")
+      }
+      else{
+        console.log("continue session")
+        showProgress()
+      }
+        
+    }, []);
 
   return (
     <React.Fragment>
@@ -94,7 +162,7 @@ const Cloud = () => {
                 <h3 id="h3id">Select DAG File:</h3>
                 <div id="tot">
                   <div id="dropd">
-                    <select className="form-select" id="value">
+                    <select className="form-select" id="value" value={value || ''} onChange={handleChange}>
                       <option value="centene_pipe">Centene</option>
                       <option value="Cigna">Cigna</option>
                       <option value="uhc_pipe">UHC</option>
@@ -111,7 +179,7 @@ const Cloud = () => {
                       className="btn btn-danger"
                       type="button"
                       id="submit-button"
-                      onClick={showProgress}
+                      onClick={true ? showProgress : checkStatus}
                     >
                       Submit
                     </button>
